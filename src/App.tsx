c5791afd281688d807b9cdae99a450334a8109c3
ouxnq <buenop.vinicius@gmail.com>
2024-08-16 16:42:41 -0300

    refactor: code refactor for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
